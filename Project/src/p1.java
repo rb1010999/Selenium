de02335b3658d4a7bc8e00a88f81f9697ec82a05
rahul.badgujar <rahul.badgujar@localhost>
2021-12-24 12:28:01 +0530

    First Commit

---
+++ Project/src/p1.java
@@ -0,0 +1,36 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class p1 {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver","//Users/rahul.badgujar/eclipse-workspace/Project Heirloom/bin/chromedriver 2");
+		WebDriver Driver = new ChromeDriver();
+		Driver.get("https://dev.heirloomproject.co/login");
+		
+		Driver.manage().window().maximize();
+		
+		Driver.findElement(By.id("email")).sendKeys("rahul101099@yopmail.com");
+		Thread.sleep(1000);
+		
+		Driver.findElement(By.id("password")).sendKeys("Rahul@101099");
+		Thread.sleep(1000);
+		
+		Driver.findElement(By.xpath("//span[normalize-space()='Login']")).click();
+		Thread.sleep(1000);
+		
+		Driver.findElement(By.xpath("//img[@alt='Collapse Menu']")).click();
+		Thread.sleep(1000);
+		
+		
+		
+		
+		
+		
+	
+	}
+
+}
